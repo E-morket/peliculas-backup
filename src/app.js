e6116580cb52483e4b668fbd9a9b7d4faaa3e392
Esteban Arguedas <arguedas0099@hotmail.com>
2023-01-27 01:10:29 -0600

    agrgamos cors

--- src/app.js
+++ src/app.js
@@ -3,6 +3,13 @@ import {DB} from './db.js'
 import{PORT} from './config.js'
 
 const app = express ()
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+  });
 
 app.get('/',async (req,res)=>{
 const rows = await DB.query('SELECT * from peliculas')
